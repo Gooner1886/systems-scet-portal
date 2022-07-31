f647859ebffb15b5df9ec759bcf87bd585217c71
nash057 <nihalraj2000@gmail.com>
2022-07-31 20:20:24 +0530

    Caraousel

--- src/components/views/LandingPage.js
+++ src/components/views/LandingPage.js
@@ -1,10 +1,15 @@
 import React from "react";
 import NavBar from "./NavBar";
+import CarouselBar from "./CarouselBar";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
 
 const LandingPage = () => {
   return (
     <>
       <NavBar />
+      <CarouselBar />
       <div>LandingPage</div>
     </>
   );
